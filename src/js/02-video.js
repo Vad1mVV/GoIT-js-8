7e1dc8459094ea3e82fb2cf5378f8f321054e5db
Vad1mVV <107924193+Vad1mVV@users.noreply.github.com>
2023-02-21 10:23:06 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
+const savedTime = JSON.parse(localStorage.getItem(CURRENT_TIME));
+
+player.on('timeupdate', throttle(onSaveTime, 5000));
+
+function onSaveTime(d) {
+    localStorage.setItem(CURRENT_TIME, JSON.stringify(d.seconds))
+}
+
+player.setCurrentTime(savedTime || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+}
+const LOCAL_NAME = 'feedback-form-state';
+const data = {};
+const parcedLocalData = JSON.parse(localStorage.getItem(LOCAL_NAME));
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+    data[e.target.name] = e.target.value;
+    localStorage.setItem(LOCAL_NAME, JSON.stringify(data));
+}
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem(LOCAL_NAME)));
+    refs.form.reset();
+}
+
+if (parcedLocalData) {
+    refs.input.value = parcedLocalData.email;
+    refs.textarea.value = parcedLocalData.message;
+    localStorage.clear();
+}
